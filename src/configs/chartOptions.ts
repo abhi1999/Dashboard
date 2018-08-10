36c9a6979b78451626e2e1e36ec172e618cbbdb5
abhishek <abhi1999@gmail.com>
2018-08-09 20:33:27 -0400

    styles changes

--- src/configs/chartOptions.ts
+++ src/configs/chartOptions.ts
@@ -16,7 +16,7 @@ export const ChartOptions = {
     },   
   }
 
-export const ChartBackgroundColors = ['#3366CC',
+export const ChartBackgroundColors1 = ['#3366CC',
         '#DC3912',
         '#FF9900',
         '#109618',
@@ -37,3 +37,24 @@ export const ChartBackgroundColors = ['#3366CC',
         '#5574A6',
         '#3B3EAC',
         ]
+export const ChartBackgroundColors =
+['#e6f1ff',
+'#cce2ff',
+'#b3d4ff',
+'#99c5ff',
+'#80b7ff',
+'#66a8ff',
+'#4d9aff',
+'#338bff',
+'#1a7dff',
+'#006eff',
+'#0063e6',
+'#0058cc',
+'#004db3',
+'#004299',
+'#003780',
+'#002c66',
+'#00214d',
+'#001e46',
+'#001633',
+'#000b1a']
