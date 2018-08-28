56a06b59e2e3b8dfb118c412fce953aa570c1bd2
abhishek <abhi1999@gmail.com>
2018-08-27 22:33:00 -0400

    bug fix

--- src/components/jsonEditView/index.tsx
+++ src/components/jsonEditView/index.tsx
@@ -1,4 +1,5 @@
 import * as  React from 'react';
+/*
 import {
     JsonTree,
     ADD_DELTA_TYPE,
@@ -7,7 +8,7 @@ import {
     DATA_TYPES,
     INPUT_USAGE_TYPES,
 } from 'react-editable-json-tree';
-
+*/
 // import ReactJson from 'react-json-view'
  
 class JsonTreeComponent extends React.Component<any, any>{
@@ -33,8 +34,9 @@ class JsonTreeComponent extends React.Component<any, any>{
                 },
             ],
         }
+        // <JsonTree data={data} readOnly={false} />
         return <React.Fragment>
-            <JsonTree data={data} readOnly={false} />
+            <div>json edit</div>
         </React.Fragment>
     }
 
